28ef277f4affa52bad42f24bb33966ef359a360b
Antti Kaihola <antti.kaihola@eniram.fi>
2017-03-01 16:11:58 +0200

    Configuration for the skeleton file path is now in notebook.json directly under the key "skeleton"

--- jupyter_skeleton/amd/index.js
+++ jupyter_skeleton/amd/index.js
@@ -7,32 +7,31 @@ define([
     var config = require('services/config');
 
     var common_options = {'base_url': utils.get_body_data('baseUrl')};
-    var cfg = new config.ConfigSection('tree', common_options);
-    // cfg.load();
+    var cfg = new config.ConfigSection('notebook', common_options);
 
     function load_ipython_extension() {
         console.info('jupyter-skeleton extension loaded');
         var contents = Jupyter.new_notebook_widget.contents;
         contents.original_new_untitled = contents.new_untitled;
-        contents.new_untitled = function (path, options) {
+        contents.new_untitled = function(path, options) {
             console.info(path, options);
             return Promise.all([cfg.load(), contents.original_new_untitled(path, options)])
                 .then(function(section_and_created) {
                     var section = section_and_created[0];
                     var created = section_and_created[1];
                     var original = contents.get(
-                        section.skeletons.notebook, {type: 'notebook'}
+                        section.skeleton, {type: 'notebook'}
                     );
-                    return Promise.all([original, Promise.resolve(created)]);
+                    return Promise.all([original, Promise.resolve(created.path)]);
                 })
-                .then(function(original_and_created) {
-                    var original = original_and_created[0];
-                    var created = original_and_created[1];
+                .then(function(original_and_path) {
+                    var original = original_and_path[0];
+                    var new_path = original_and_path[1];
                     var save_options = {
                         content: original.content,
                         type: original.type
                     };
-                    return contents.save(created.path, save_options);
+                    return contents.save(new_path, save_options);
                 });
         };
     }
